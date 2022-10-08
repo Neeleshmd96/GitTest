c3de50d7a84614e339510a4d158b5cf06b409c00
Neeleshmd96 <neeleshd.xworkz@gmail.com>
2022-10-08 15:45:36 +0530

    Final  push

---
+++ GitTest/src/com/neel/gittest/Sample.java
@@ -0,0 +1,14 @@
+package com.neel.gittest;
+
+public class Sample {
+
+    private int num;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
